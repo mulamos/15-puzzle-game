281c45329b66e7bca1526f5302d8bb5b5c4a2e1c
Wonakee Amos <mulamos@Wonakees-MacBook.local>
2016-11-02 19:10:47 -0500

    basic requirements, working

--- fifteen.js
+++ fifteen.js
@@ -1,104 +1,92 @@
 $(document).ready (function() {
 
-	var gameTiles = [];
-	var blankSpace = [];
+	$("#puzzlearea div").addClass("puzzlepiece");
 
-	var gameTiles = $("#puzzlearea div");
-	var row = 0;
-	var right = 0;
-	var top = 0;
+	var posx = parseInt($("#puzzlearea").css("top"));
+	var posy = parseInt($("#puzzlearea").css("left"));
 
-	if (gameTiles.length) {
+	var jpgx = 0;
+	var jpgy = 0;
 
-		for (var i = 0; i < gameTiles.length; i++) {
-			$(gameTiles[i]).addClass("puzzlepiece");
-			$(gameTiles[i]).css({"float":"left"});
-			$(gameTiles[i]).css({"backgroundSize":"400px 400px"});
+	var clearx = 300;
+	var cleary = 300;
 
-			blankSpace[i] = [];
-			blankSpace[i][0] = right;
-			console.log(blankSpace[i][0]);
-			/*console.log(blankSpace[i]);
-			console.log(blankSpace[i][0]);*/
-			blankSpace [i][1] = top;
-			console.log(blankSpace[i][1]);
-			/*console.log(blankSpace);
-			console.log(blankSpace[i]);
-			console.log(blankSpace[i][1]);*/
-			$(gameTiles[i]).css({"backgroundPosition":"-"+blankSpace[i][0]+"px -"+blankSpace[i][1]+"px"});
-			row++;
-			console.log(row);
-			if (row === 4) {
-				top += 100; 
-				right = 0; 
-				row = 0;
-			} 
-			else {
-				right += 100;
+	var gameTile = document.getElementsByClassName("puzzlepiece");
+
+	if (gameTile.length) {
+
+		for (var i = 0; i < gameTile.length; i++) {
+			$(gameTile[i]).css("backgroundSize", "400px 400px");
+
+
+			$(gameTile[i]).css("background-position", jpgx+"px "+jpgy+"px");
+			jpgx -= 100;
+			if (jpgx%400 == 0) {jpgy -= 100;}
+
+			$(gameTile[i]).css("top", posy);
+			$(gameTile[i]).css("left", posx);
+
+			posx += 100;
+
+			if (i != 0 && (i+1)%4 == 0) {
+				posy += 100;
+				posx = parseInt($("#puzzlearea").css("top"));
 			}
+
+			$(gameTile[i]).mouseover(function() {
+				if (isValid(this)) { $(this).addClass("movablepiece"); } 
+			});
+
+			$(gameTile[i]).mouseleave(function(){
+				$(this).removeClass("movablepiece");
+			});
+
+			$(gameTile[i]).click(function() {
+				if (isValid(this)) {
+				swapTile(this);
+				}
+			});
 		}
 	}
+	
+	var isValid = function(gameTile) {
+		if(((parseInt($(gameTile).css("top")) - cleary == 100 || parseInt($(gameTile).css("top")) - cleary == -100) && parseInt($(gameTile).css("left")) - clearx == 0) ||
+		((parseInt($(gameTile).css("left")) - clearx == 100 || parseInt($(gameTile).css("left")) - clearx == -100) && parseInt($(gameTile).css("top")) - cleary == 0)) {
+			return true;
+		}
+		else { return false; }
+	};
 
-	var square = function(row, column) {
-		if (row === 1) {
-			var result = row*column;
-			$("#puzzlearea div:nth-child(result)").attr("id", "square_"+row+"_"+column);
+	var swapTile = function(gameTile) {
+		var tempx = clearx;
+		var tempy = cleary;
+
+		cleary = parseInt($(gameTile).css("top"));
+		clearx = parseInt($(gameTile).css("left"));
+
+		$(gameTile).css("top", tempy);
+		$(gameTile).css("left", tempx);
+	};
+
+	$("#shufflebutton").click(function() {
+		var amt = Math.floor(Math.random()*100);
+
+		for (var i=0; i < amt; i++) {
+			movetile();
+		}
+	});
+
+	var movetile = function() {
+		var arr = [];
+
+		for (var i=0; i < gameTile.length; i++) {
+			if (isValid(gameTile[i])) {
+				arr.push(gameTile[i]);
+			}
 		}
 
-		else if (row === 2 && column === 1) {
-			var result = (row*column)+3;
-			$("#puzzlearea div:nth-child(result)").attr("id", "square_"+row+"_"+column);
-		}
-			else if (column === 2) {
-				var result = (row*column)+2;
-				$("#puzzlearea div:nth-child(result)").attr("id", "square_"+row+"_"+column);
-			}
-			else if (column === 3) {
-				var result = (row*column)+1;
-				$("#puzzlearea div:nth-child(result)").attr("id", "square_"+row+"_"+column);
-			}
-			else {
-				var result = row*column;
-				$("#puzzlearea div:nth-child(result)").attr("id", "square_"+row+"_"+column);
-			}
+		var move = arr[Math.floor(Math.random() * arr.length)];
 
-		if (row === 3 && column === 1) {
-			var result = (row*column)+6;
-			$("#puzzlearea div:nth-child(result)").attr("id", "square_"+row+"_"+column);
-		}
-			else if (column === 2) {
-				var result = (row*column)+4;
-				$("#puzzlearea div:nth-child(result)").attr("id", "square_"+row+"_"+column);
-			}
-			else if (column === 3) {
-				var result = (row*column)+2;
-				$("#puzzlearea div:nth-child(result)").attr("id", "square_"+row+"_"+column);
-			}
-			else
-			{
-				var result = row*column;
-				$("#puzzlearea div:nth-child(result)").attr("id", "square_"+row+"_"+column);
-			}
-
-		if (row === 4 && column === 1) {
-			var result = (row*column)+9;
-			$("#puzzlearea div:nth-child(result)").attr("id", "square_"+row+"_"+column);
-		}
-			else if (column === 2) {
-				var result = (row*column)+6;
-				$("#puzzlearea div:nth-child(result)").attr("id", "square_"+row+"_"+column);
-			}
-			else if (column === 3) {
-				var result = (row*column)+3;
-				$("#puzzlearea div:nth-child(result)").attr("id", "square_"+row+"_"+column);
-			}
-			else {
-				var result = row*column;
-				$("#puzzlearea div:nth-child(result)").attr("id", "square_"+row+"_"+column);
-			}
-	}
-
-	var moveable = function(square) {
-		
-	}
+		swapTile(move);
+	};
 });	
